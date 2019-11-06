d3bb983ef306ae5226c3e57693079c7d873478eb
Thomas Castleman <contact@draugeros.org>
2019-11-06 17:37:00 -0500

    set up basic version and help flags
     * passing -v or --version will print the version number
     * passing -h or --help will print a help message
    
    Also set up system and argv to be imported since they will likely be needed later.

--- main.py
+++ main.py
@@ -24,8 +24,27 @@
 #This is the main file. We can call other files if we need to, but try to just define functions instead
 
 #Import the Modules we need, preferably with only the functions and classes we need to reduce RAM usage
+#use the system() command to run outside scripts
+from os import system
+#use argv to take input on the command line
+from sys import argv
 
 #Define Functions
 
-#Do all the I/O and processing we need
+#Define Initial Variables
+#Version of the project. Any time you make a major change, bump the number by one.
+VERSION = "0.0.1-alpha1"
+#Use argc so we don't go over the length of argv
+argc = len(argv)
 
+#Do all the I/O and processing we need
+try:
+	if (argc > 1):
+		if (argv[1] == "-h" or argv[1] == "--help"):
+			print("\nFor now, main.py does nothing other than print this help dialog and print the current version.")
+		elif (argv[1] == "-v" or argv[1] == "--version"):
+			print("\nCSCI-183 Project Version %s" % (VERSION))
+		else:
+			print("\nOption not supported. Please try '-h' or '--help'.")
+except:
+	print("\nAn error has occured.")
