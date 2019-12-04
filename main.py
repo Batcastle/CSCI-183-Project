cd1c3ca798489b1badf03d91953da81dc1e59a84
Thomas Castleman <contact@draugeros.org>
2019-12-03 23:08:41 -0500

    merge contents of masterlist function.py into main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def create_matrix(node_count):
 			matrix[each].append(1)
 	for each in range(0,top_side):
 		matrix[each][each] = 0
-			
+
 	return(matrix)
 
 
@@ -160,6 +160,31 @@ def parser(A,start,end):
             row=row+1
     print(path)
 
+#function to take easiest path and return it. Returns False if there is no valid path
+def pathfinder(A,start,end):
+    row=0
+    col=start   #initialize row and column indices
+    noback=[]   #list that saves columns that cannot be returned to
+    path=[]     #list of indices traversed, aka path taken
+    try:
+        while True:
+            if col == end:
+                break
+            for i in noback:
+                if row==i:
+                    row=row+1
+            if A[row][col]>0:
+                path.append([row,col])
+                noback.append(col)
+                col=row
+                row=0
+            else:
+                row=row+1
+    except:
+        return(False)
+    return(path)
+
+
 ##########################################################################
 
 #Define Initial Variables
@@ -186,6 +211,19 @@ try:
 		else:
 			print("\nOption not supported. Please try '-h' or '--help'.")
 	else:
-		print("\nNo options passed. Exiting . . .")
+		A[path[len(path)-1][0]][path[len(path)-1][1]]=0 #sets the last node used to 0
+		#attempt to create a list of possible paths.
+		masterlist=[]
+		while True:
+			storedpath=pathfinder(A,0,2)  #runs the pathfinder function
+			if storedpath==False:         #if there is no possible path, pathfinder returns False and loop ends
+				break
+			else:                         #here's where im hitting issues. this part should add paths to 'masterlist' but doesn't seem to be
+				masterlist.append(storedpath)
+				A[storedpath[len(storedpath)-1][0]][storedpath[len(storedpath)-1][1]]=0
+				print(storedpath)
+				continue
+
+
 except:
 	print("\nAn error has occured.")
